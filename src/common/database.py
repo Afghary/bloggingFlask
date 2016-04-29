31b48930679d791d8ba4dec994b94bfaf186be8e
Mohsen <mcmfarahanchi@gmail.com>
2016-04-29 17:38:47 +0500

    prep

---
+++ src/common/database.py
@@ -0,0 +1,30 @@
+import pymongo
+
+__author__ = 'Mohsen'
+
+
+class Database(object):
+    # static property
+
+    # Universal Resource Identifier
+    URI = "mongodb://127.0.0.1:27017"
+    DATABASE = None
+
+    @staticmethod
+    def initialize( ):
+        client = pymongo.MongoClient(Database.URI)
+        Database.DATABASE = client[ 'fullstack' ]
+
+    @staticmethod
+    def insert( collection, data ):
+        Database.DATABASE[ collection ].insert(data)
+
+    @staticmethod
+    def find( collection, query ):
+        return Database.DATABASE[ collection ].find(query)
+
+    @staticmethod
+    def find_one( collection, query ):
+        # find_one method returns the first json found
+        # despite find witch returns a cursor object.
+        return Database.DATABASE[ collection ].find_one(query)
