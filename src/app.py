76b4e76d479b98e3684ae6c011cc67fb5bda4dc7
Mohsen <mcmfarahanchi@gmail.com>
2016-05-02 19:12:32 +0500

    semi final commit

--- src/app.py
+++ src/app.py
@@ -1,11 +1,62 @@
-from flask import Flask
+from flask import Flask, render_template, request, session
+from src.common.database import Database
+from src.models.user import User
 
 app = Flask(__name__)
+app.secret_key = "jose"
 
 
-@app.route('/') # www.mysite.com/api/
+@app.route('/')  # www.mysite.com/api/
 def hello_world( ):
-    return 'Hello World!'
+    return render_template('login.html')
+
+
+@app.before_first_request
+def initialize_database( ):
+    Database.initialize()
+
+@app.route('/login')
+def login_template():
+    return render_template('login.html')
+
+@app.route('/register')
+# www.mysite.com/api/register
+def register_template():
+    return render_template('register.html')
+
+@app.route("/auth/login", methods=[ 'POST' ])
+def login_user( ):
+    email = request.form[ 'email' ]
+    password = request.form[ 'password' ]
+
+    if User.login_valid(email, password):
+        User.login(email)
+    else:
+        session[ 'email' ] = None
+
+    return render_template('profile.html', email=session[ 'email' ])
+
+
+@app.route('/auth/register', methods=[ 'POST' ])
+def register_user( ):
+    email = request.form[ 'email' ]
+    password = request.form[ 'password' ]
+
+    User.register(email, password)
+
+    return render_template('profile.html', email=session[ 'email' ])
+
+@app.route('/blogs/<string:user_id>')
+@app.route('/blogs')
+def user_blogs(user_id=None):
+    if user_id is not None:
+        user = User.get_by_id(user_id)
+    else:
+        user = User.get_by_email(email=session['email'])
+    blogs = user.get_blogs()
+
+    return render_template("user_blogs.html",blogs=blogs, email = user.email)
 
 if __name__ == '__main__':
-    app.run(port=4995)
+    # app.debug
+    app.run(port=4995, debug=True)
